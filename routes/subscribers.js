aa26160faf7f675d0bfc64b1bda5b6147d5d1223
lokesh <lokesh95664@gmail.com>
2023-05-07 21:48:33 +0530

    Completed Getting one user id!

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -14,10 +14,12 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message : err.message})
     }
 })
+
 //Getting one
-router.get('/:id', (req,res)=>{
-    res.send(req.params.id)
+router.get('/:id', getSubscriber, (req,res)=>{
+    res.json(res.subscriber)
 })
+
 //Creating one
 router.post('/', async (req, res)=>{
 
@@ -34,13 +36,43 @@ router.post('/', async (req, res)=>{
         res.status(400).json({ message : err.message})
       }
 })
+
+
 //Updating one
 router.patch('/:id', (req, res)=>{
 
 })
-//Deleting one
-router.delete('/:id', (req, res)=>{
 
+
+//Deleting one
+router.delete('/:id', getSubscriber, async (req, res)=>{
+    
+    try {
+         await res.subscriber.remove()
+         res.json(" Deleted Subscriber!")
+
+    } catch (err) {
+        res.status(500).json({ message : err.message})
+        
+    }
+    
 })
+
+async function getSubscriber( req, res,  next){
+    let subscriber
+    try {
+        subscriber =  await  Subscriber.findById(req.params.id);
+        if(subscriber === null){
+            return res.status(404).json({ message : "id not found!"})
+        }
+
+
+    } catch (err) {
+        res.status(500).json({ message : err.message });
+    }
+
+    res.subscriber = subscriber
+    next();
+}
 
 module.exports = router;
